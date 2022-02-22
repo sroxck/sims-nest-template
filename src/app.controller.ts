f16f936e62f6f59097c0d3ee9f0f194f6ed15a4f
sroxck <642261686@qq.com>
2022-02-22 17:32:38 +0800

    ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,17 @@
+/*
+ * @Author: Jingdongdong
+ * @Date: 2022-02-11 18:05:51
+ * @LastEditTime: 2022-02-22 17:32:29
+ * @LastEditors: Jingdongdong
+ * @Description: 
+ */
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ApiTags } from '@nestjs/swagger'
 @Controller()
 @ApiTags('全局')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {} 
 
   @Get()
   getHello(): string {
